3ebd47df7c10d1b07b1bf4bf02fa877bdd7dedc6
amri1978 <amine.amri@gmail.com>
2019-11-18 00:54:12 +0100

    Update action-robot-commander.py

--- action-robot-commander.py
+++ action-robot-commander.py
@@ -23,8 +23,7 @@ API_KEY = "api_key"
 _id = "snips-skill-robot-commander"
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-
-#Amine 16h54
+#Message Format : (RGHT|LEFT|FRWD|BKWD):(S|M|F):Distance in cm or angle in degree
 class Skill_RobotCommander:
     def __init__(self):
 
@@ -45,22 +44,44 @@ class Skill_RobotCommander:
     # section -> extraction of slot value
 
 
+    def extract_angle(self, intent_message, default_angle):
+        angle = default_angle
+        if intent_message.slots.angle:
+            angle = intent_message.slots.angle.first().value
+        if angle < 0:
+            angle = 0
+        if angle > 360:
+            angle = 360
+        return angle
+        
     def extract_distance(self, intent_message, default_distance):
         distance = default_distance
         if intent_message.slots.distance:
             distance = intent_message.slots.distance.first().value
         if distance < 0:
             distance = 0
-        if distance > 100:
-            distance = 100
         return distance
-
+        
+    def extract_speed(self, intent_message, default_speed):
+        speed = default_speed
+        if intent_message.slots.speed:
+            speed = intent_message.slots.speed.first().value
+            if speed == 'fast':
+                speed = 'F'
+            elif speed == 'slow':
+                speed = 'S'
+            else:
+                speed = 'M'        
+        print (speed)
+        return speed
+   
 
     # section -> handlers of intents
 
     def callback(self, hermes, intent_message):
         print("[COMMAND] Received")
         intent_name = intent_message.intent.intent_name
+        print(intent_name)
         if ':' in intent_name:
             intent_name = intent_name.split(":")[1]
         if intent_name == 'forward':
@@ -73,32 +94,34 @@ class Skill_RobotCommander:
             self.queue.put(self.right(hermes, intent_message))
 						
     def forward(self, hermes, intent_message):
-        percent = self.extract_percentage(intent_message, None)
-        if percent is None:
-            self.terminate_feedback(hermes, intent_message)
-            return
-        ser.write(b'FORWARD\n')
+        distance = self.extract_distance(intent_message, 10)
+        speed = self.extract_speed(intent_message, 'M')
+        command = b"".join(['FRWD:', speed, ':',str(int(distance)),'\n'])
+        print(command)
+        ser.write(command)
         self.terminate_feedback(hermes, intent_message)
 		
     def backward(self, hermes, intent_message):
-        percent = self.extract_percentage(intent_message, None)
-        if percent is None:
-            self.terminate_feedback(hermes, intent_message)
-            return
-        ser.write(b'BACKWARD\n')
+        distance = self.extract_distance(intent_message, 10)
+        speed = self.extract_speed(intent_message, 'M')
+        command = b"".join(['BKWD:', speed, ':',str(int(distance)),'\n'])
+        print(command)
+        ser.write(command)
         self.terminate_feedback(hermes, intent_message)
 				
     def left(self, hermes, intent_message):
-        ser.write(b'LEFT\n')
+        angle = self.extract_angle(intent_message, 45)
+        command = b"".join(['LEFT:S:',str(int(angle)),'\n'])
+        ser.write(command)
         self.terminate_feedback(hermes, intent_message)
 
     def right(self, hermes, intent_message):
-        ser.write(b'RIGHT\n')
+        angle = self.extract_angle(intent_message, 45)
+        command = b"".join(['RGHT:S:',str(int(angle)),'\n'])
+        ser.write(command)
         self.terminate_feedback(hermes, intent_message)
 
     
-
-    # section -> feedback reply // future function
     def terminate_feedback(self, hermes, intent_message, mode='default'):
         if mode == 'default':
             hermes.publish_end_session(intent_message.session_id, "")
